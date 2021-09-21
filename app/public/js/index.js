be8becccd3dd768865b10709c5b691ffb7d7f2ac
Pallavi Ratra <pallaviratra75@gmail.com>
2021-09-21 14:08:20 -0400

    Add dataJS API

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -4,21 +4,30 @@ const User = {
         "user": {},
         }
     },
-    created() {
-        console.log("A");
-        fetch('https://randomuser.me/api/')
-        .then( response => response.json() )
-        .then( (responseJson) => {
-            console.log(responseJson);
-            console.log("C");
-            this.user = responseJson.results[0];
-        })
-        .catch( (err) => {
-            console.error(err);
-        })
-        console.log("B");
-    } //end created
-} // end Offer config
+
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.user.dob.date)
+            .format('DD MMM YYYY')
+        }
+    },
+
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                this.user = responseJson.results[0];
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+    },
+
+    created(){
+        this.fetchUserData();
+    }
+}
   
 Vue.createApp(User).mount('#randomUser');
-console.log("Z");
